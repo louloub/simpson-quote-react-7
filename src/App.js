1a79b1744938ffe5dd4b695b2d572b0b2b791bbe
loulou <yourdj.france@gmail.com>
2020-10-26 17:02:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const sampleSimpson = [
+  {
+    "quote": "By chilling my loins I increase the chances of impregnating my wife.",
+    "character": "Apu Nahasapeemapetilon",
+    "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+    "characterDirection": "Left"
+  }
+]
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      simpson: sampleSimpson
+    };
+    
+    this.getSimpson = this.getSimpson.bind(this);
+    // This ensures that inside getEmployee, this actually refers to the component instance.
+  }
+
+  getSimpson() {
+    // Send the request
+    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes?count=num')
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {
+        this.setState({
+          simpson: data.results[0],
+        });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <DisplayEmployee employee={this.state.employee} />
+        <button type="button" onClick={this.getEmployee}>Get employee</button>
+      </div>
+    );
+  }
 }
 
 export default App;
